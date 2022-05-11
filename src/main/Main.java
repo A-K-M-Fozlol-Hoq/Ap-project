cbf0e9b564a5b6b703bfdebf56794659d10a7822
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-05-12 04:55:46 +0600

    project initialized

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,40 @@
 package main;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-//		variables
 		char willLoopContinue ='y';
 		int userType=0;
 		
 		PrintMessage.printIntro();
 		
 		for (; willLoopContinue =='y' || willLoopContinue =='Y'; ) {
-			System.out.println("        1)Student log in.               2)Teacher log in.         3)Admin log in.");
-			System.out.println("Please enter your option: ");
-			userType = scan.nextInt();
+			PrintMessage.printLoginType();
+			
+			try {
+				userType = scan.nextInt();
+	        } catch (InputMismatchException exception) {
+	            System.out.println("Not an integer, please try again.");
+	        }
+			
+			
+			switch(userType) {
+			  case 1:
+			    // code block
+			    break;
+			  case 2:
+				    // code block
+				    break;
+			  case 3:
+				    // code block
+				    break;
+			  default:
+				  System.out.println("Please enter valid option.");
+			}
 			
 //			do all stuff
 			System.out.println("Please enter y or Y to continue again otherwise enter anything else: ");
--- src/main/PrintMessage.java
+++ src/main/PrintMessage.java
@@ -14,4 +14,15 @@ public class PrintMessage {
 		System.out.println(".               ***************Stamford University Bangladesh******************");
 		System.out.println(".             ******************* Project Management System ***********************");
 	}
+	
+	public static void printLoginType() {
+		System.out.println("        1)Student log in.               2)Teacher log in.         3)Admin log in.");
+		System.out.println("Please enter your option: ");
+	}
+
+	public static void printAdminMenu() {
+		System.out.println("Please enter your option: ");
+		System.out.println("1)Create a new class. \n2)Add teacher. \\n3)Change User name. \n4)Change Password.");
+		System.out.println("\n5)Logout.");
+	}
 }
